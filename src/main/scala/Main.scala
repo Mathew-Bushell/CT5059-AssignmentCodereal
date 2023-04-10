eaff9f4e9c923bd8027afede081d4fd87b5af9f6
Mathew-Bushell <mathewbushell@live.co.uk>
2023-04-10 17:39:25 +0100

    pain

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,15 @@
 import scala.io.Source
-import scala.collection.mutable.ListBuffer
-import scala.collection.mutable.Map
+//import scala.collection.mutable.ListBuffer
+import scala.collection.mutable
+//import scala.collection.mutable.Map
 import scala.collection.mutable.ListMap
 object Main {
   def main(args:Array[String]):Unit = {
     val connectives: List[String] = List("a", "an", "the", "for", "and", "nor", "but", "or", "yet", "so", "to", "at", "by", "from", "in", "into", "of", "on", "onto", "with", "as", "than", "up", "down", "off", "out", "over", "under", "above", "about", "after", "around", "before", "through", "throughout", "therefore", "nonetheless", "whereas", "finally", "furthermore", "until", "upon", "within", "am", "thus", "however")
     val flagWords: List[String] = List("threat", "vulnerabilities", "attack", "loopholes", "loophole", "whitelist", "blacklist", "hash", "encryption")
     val emailFile = Source.fromFile("Email.txt")
-    val filteredEmail = new ListBuffer[String]()
-    val words = new ListBuffer[String]()
+    val filteredEmail = new mutable.ListBuffer[String]()
+    val words = new mutable.ListBuffer[String]()
     var wordFrequency = scala.collection.mutable.Map[String, Int]()
 
     for (line <- emailFile.getLines) {
@@ -55,12 +56,13 @@ object Main {
     for (word <- wordss) {
       wordFrequency += (word -> filteredEmailList.count(_ == word))
     }
-//    val wordFrequency = wordFrequency.toMap()
-    val wordFrequency2 = ListMap(wordFrequency.toSeq.sortWith(_._2>_._2):_*)
+    val wordFrequency2 = wordFrequency.toSeq
+//    val wordFrequency3 = ListMap(wordFrequency2.toSeq.sortWith(_._2>_._2):_*)
 //    println(wordFrequency2.keys)
 //    println(wordFrequency2.values)
     println(wordFrequency2)
     var susScore = 0
+    //take an int value for each map item and "blacklist" selected ones that are already in the top 5
     for(x <- filteredEmailList){
       for (flag <- flagWords){
         if(x == flag){
